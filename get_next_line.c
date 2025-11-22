27c759b51a977afa5994be44a0a5b5ae1cd922d8
maryvi <maryvi.jobfinder@gmail.com>
2025-11-22 22:56:45 +0000

    uso variable estatica para size de malloc

--- get_next_line.c
+++ get_next_line.c
@@ -4,37 +4,52 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-static	int	count_bytes(unsigned char *buffer)
+
+static char *line_maker(const char *buffer, char *line, int size)
 {
+	static int	buffer_size;
+	char	*new_line;
 	int	i;
+	int	j;
 
+	buffer_size += size;
 	i = 0;
-	while (buffer[i])
-		i++;
-	return (i);
-}
-
-static int	line_maker(const char *buffer, int size)
-{
-	static int buffer_size;
-
-	buffer_size += buffer_size;
-	unsigned char	*read;
-	int	bytes;
-
-	bytes = count_bytes(*line)
-
+	new_line = malloc(buffer_size);
+	if (line != 0)
+	{
+		while (line[i])
+		{
+			new_line[i] = line[i];
+			i++;
+		}
+		free(line);
+	}
+	j = 0;
+	while (buffer[j])
+	{
+		new_line[i + j] = buffer[j];
+		j++;
+	}
+	return new_line;
 }
 
 int	main(void)
 {
 	int	fd;
-	unsigned char	*buffer;
+	char	*buffer;
+	char	*line;
 
 	buffer = malloc(BUFFER_SIZE);
 	fd = open("file.txt", O_RDONLY);
 	read(fd, (void *)buffer, BUFFER_SIZE);
-	printf("%s\n", buffer);
+	line = line_maker(buffer, 0, BUFFER_SIZE);
+	printf("buffer:%s\nline:%s\n", buffer, line);
+	read(fd, (void *)buffer, BUFFER_SIZE);
+	line = line_maker(buffer, line, BUFFER_SIZE);
+	printf("buffer:%s\nline:%s\n", buffer, line);
+	read(fd, (void *)buffer, BUFFER_SIZE);
+	line = line_maker(buffer, line, BUFFER_SIZE);
+	printf("buffer:%s\nline:%s\n", buffer, line);
 	free(buffer);
 	return (0);
 }
