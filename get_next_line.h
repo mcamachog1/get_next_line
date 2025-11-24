2c4c6c0a5ddc524485a3e8741adf6b90f2ff473b
Maryvi Camacho <macamach@c1r7s4.42porto.com>
2025-11-24 16:40:18 +0000

    separar utils y makefile

--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,18 @@
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
+# define BUFFER_SIZE 80
 #endif
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 
 typedef	struct	s_line
 {
 	unsigned char	*line;
 	struct s_line	*next;
 } t_line;
+
+char	*line_maker(const char *buffer, char *line, int size);
+void	truncate_buffer(char *buffer, int size);
+int	find_nl_index(const char *buffer);
 
 #endif
