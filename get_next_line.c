35c8fd0aa73ca7e301a05ed70bede50d939760bb
maryvi <maryvi.jobfinder@gmail.com>
2025-11-24 08:16:13 +0000

    antes de buscar fin de linea

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "get_next_line.h"
 
-static char *line_maker(const char *buffer, char *line, int size)
+char *line_maker(const char *buffer, char *line, int size)
 {
 	static int	buffer_size;
 	char	*new_line;
@@ -24,21 +25,12 @@ static char *line_maker(const char *buffer, char *line, int size)
 		free(line);
 	}
 	j = 0;
-	while (buffer[j])
+	while (buffer[j] && j < BUFFER_SIZE)
 	{
 		new_line[i + j] = buffer[j];
 		j++;
 	}
 	return new_line;
-}
-
-static void clear_buffer(char *buffer)
-{
-	while(*buffer)
-	{
-		*buffer = ' ';
-		buffer++;
-	}
 }
 
 static void truncate_buffer(char *buffer, int size)
@@ -54,9 +46,9 @@ int	main(void)
 	int	bytes_read;
 
 	buffer = malloc(BUFFER_SIZE);
-	line = 0;
 	fd = open("file.txt", O_RDONLY);
-	bytes_read = read(fd, (void *)buffer, BUFFER_SIZE);
+	line = 0;
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	while (bytes_read > 0)
 	{
 		if (bytes_read < BUFFER_SIZE)
@@ -64,13 +56,11 @@ int	main(void)
 		line = line_maker(buffer, line, BUFFER_SIZE);
 		printf("BUFFER_SIZE:%d\nBYTES_READ:%d\n", BUFFER_SIZE, bytes_read);
 		printf("**** buffer *****\n");
-		printf("%s\n", buffer);
+		printf("%.*s\n", BUFFER_SIZE, buffer);
 		printf("**** line   *****\n");
-		printf("%s\n", line);
-		bytes_read=read(fd, (void *)buffer, BUFFER_SIZE);
+		printf("%.*s\n", (int)(strlen(line) - 1), line);
+		bytes_read=read(fd, buffer, BUFFER_SIZE);
 	}
-	if (bytes_read == 0)
-		truncate_buffer(buffer, 0);
 	if (bytes_read == -1)
 		printf("Error\n");
 	free(buffer);
